afc595d274ba6d2be6a15c091636da498fbb194e
Adam <adamaizal4477@gmail.com>
2024-09-22 01:54:26 +1200

    Initial Commit

---
+++ SubscriptionTracker/SubscriptionTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptionTrackerApp.swift
+//  SubscriptionTracker
+//
+//  Created by Adam Aizal on 22/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SubscriptionTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SubscriptionTrackerTests/SubscriptionTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptionTrackerTests.swift
+//  SubscriptionTrackerTests
+//
+//  Created by Adam Aizal on 22/09/2024.
+//
+
+import Testing
+@testable import SubscriptionTracker
+
+struct SubscriptionTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
